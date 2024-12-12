90c3f63f22b08b21d8b33d7ff2fcb135be98049c
rhdehgks <rhdehgks@naver.com>
2024-12-12 21:52:37 +0900

    feat: DTO 객체 수정 가능하도록 변경

--- app/dto/base.py
+++ app/dto/base.py
@@ -4,6 +4,5 @@ from pydantic import BaseModel
 class BaseDTO(BaseModel):
     class Config:
         from_attributes = True
-
-    def __call__(self, Entity):
-        return Entity(**self.model_dump())
+        frozen = False
+        extra = "allow"
