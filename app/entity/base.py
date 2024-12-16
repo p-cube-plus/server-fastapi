20d0c77a1589eed108682c159fd3e6e0ab2f4351
reudekx <reudekx@gmail.com>
2024-12-16 13:06:48 +0900

    feat: metaclass를 이용한 Entity -> DTO 변환 로직 추가

--- app/entity/base.py
+++ app/entity/base.py
@@ -1,5 +1,14 @@
+from pydantic import BaseModel
 from sqlalchemy.orm import DeclarativeBase
 
 
-class BaseEntity(DeclarativeBase):
+class EntityMeta(type(DeclarativeBase)):
+    def __call__(cls, *args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], BaseModel):
+            instance = cls(**args[0].model_dump(exclude_unset=True))
+            return instance
+        return super().__call__(*args, **kwargs)
+
+
+class BaseEntity(DeclarativeBase, metaclass=EntityMeta):
     pass
