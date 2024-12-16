86640c1c3c535f21a86a962957511e7d0efbdf6b
reudekx <reudekx@gmail.com>
2024-12-16 13:07:41 +0900

    feat: Entity -> DTO 변환 테스트 코드 추가

--- tests/unit/test_model_convert.py
+++ tests/unit/test_model_convert.py
@@ -27,3 +27,24 @@ async def test_convert_entity_to_dto():
     common_keys = set(entity_dict.keys()) & set(dto_dict.keys())
     for key in common_keys:
         assert entity_dict[key] == dto_dict[key]
+
+
+@pytest.mark.asyncio(scope="session")
+async def test_convert_dto_to_entity():
+    async for db in MySQLDatabase():
+        user_repo = UserRepository(db)
+        user_entity = (await user_repo.find_all())[0]
+
+    user_dto = UserRead(user_entity)
+    user_entity = UserEntity(user_dto)
+
+    entity_dict = {
+        c.key: getattr(user_entity, c.key)
+        for c in inspect(user_entity).mapper.column_attrs
+    }
+
+    dto_dict = user_dto.model_dump()
+
+    common_keys = set(entity_dict.keys()) & set(dto_dict.keys())
+    for key in common_keys:
+        assert entity_dict[key] == dto_dict[key]
