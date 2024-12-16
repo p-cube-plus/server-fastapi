3cce9d28c07dba1b0210ae74869c477c93b9127b
reudekx <reudekx@gmail.com>
2024-12-16 12:37:21 +0900

    feat: metaclass를 이용한 DTO -> Entity 변환 로직 추가

--- app/dto/base.py
+++ app/dto/base.py
@@ -1,8 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+from app.entity.base import BaseEntity
 
 
-class BaseDTO(BaseModel):
-    class Config:
-        from_attributes = True
-        frozen = False
-        extra = "allow"
+class DTOMeta(type(BaseModel)):
+    def __call__(cls, *args, **kwargs):
+        if len(args) == 1 and isinstance(args[0], BaseEntity):
+            instance = cls.model_validate(args[0])
+            return instance
+        return super().__call__(*args, **kwargs)
+
+
+class BaseDTO(BaseModel, metaclass=DTOMeta):
+    model_config = ConfigDict(from_attributes=True, frozen=False, extra="allow")
