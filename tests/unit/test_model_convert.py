6d3d08c3822eaa7fa3b248d82411d97a070f2c3e
reudekx <reudekx@gmail.com>
2024-12-21 01:21:40 +0900

    feat: Repository 자동 형변환 고려하여 테스트 코드 수정

--- tests/unit/test_model_convert.py
+++ tests/unit/test_model_convert.py
@@ -10,32 +10,11 @@ from app.repository.user import UserRepository
 
 
 @pytest.mark.asyncio(scope="session")
-async def test_convert_entity_to_dto():
-    async for db in MySQLDatabase():
-        user_repo = UserRepository(db)
-        user_entity = (await user_repo.find_all())[0]
-
-    user_dto = UserRead(user_entity)
-
-    entity_dict = {
-        c.key: getattr(user_entity, c.key)
-        for c in inspect(user_entity).mapper.column_attrs
-    }
-
-    dto_dict = user_dto.dict()
-
-    common_keys = set(entity_dict.keys()) & set(dto_dict.keys())
-    for key in common_keys:
-        assert entity_dict[key] == dto_dict[key]
-
-
-@pytest.mark.asyncio(scope="session")
 async def test_convert_dto_to_entity():
     async for db in MySQLDatabase():
         user_repo = UserRepository(db)
-        user_entity = (await user_repo.find_all())[0]
+        user_dto = (await user_repo.get_all())[0]
 
-    user_dto = UserRead(user_entity)
     user_entity = UserEntity(user_dto)
 
     entity_dict = {
