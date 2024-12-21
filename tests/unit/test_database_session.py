3eac74901cb560363ea63a499474b4b9ae46e602
reudekx <reudekx@gmail.com>
2024-12-22 04:04:29 +0900

    feat: DatabaseSession에 대한 테스트 코드 추가

--- tests/unit/test_database_session.py
+++ tests/unit/test_database_session.py
@@ -3,15 +3,19 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database.base import DatabaseSession
 from app.database.mysql import MySQLDatabase
+from app.database.sqlite import SQLiteDatabase
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio(scope="session")
 async def test_database_session():
     async for session in MySQLDatabase():
         assert isinstance(session, DatabaseSession)
-        print(type(session))
         assert type(session) == DatabaseSession
 
     async for session in DatabaseSession():
+        assert isinstance(session, DatabaseSession)
+        assert type(session) == DatabaseSession
+
+    async for session in DatabaseSession(SQLiteDatabase):
         assert isinstance(session, DatabaseSession)
         assert type(session) == DatabaseSession
