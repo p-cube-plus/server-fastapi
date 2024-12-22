555ff74e8a59f5d15a222d296d7e65a4551dcaca
reudekx <reudekx@gmail.com>
2024-12-22 15:38:51 +0900

    refactor: Partial 함수 이용하도록 변경

--- app/dto/user.py
+++ app/dto/user.py
@@ -1,6 +1,11 @@
+from dataclasses import Field
 from datetime import date
 
-from .base import BaseDTO
+from .base import BaseDTO, Partial
+
+
+class UserID(BaseDTO):
+    id: str
 
 
 class UserBase(BaseDTO):
@@ -32,43 +37,17 @@ class UserBase(BaseDTO):
     api_access_level: int = 0
 
 
-class UserRead(UserBase):
-    id: str
-
-
-class UserCreate(UserBase):
+class UserRead(UserBase, UserID):
     pass
 
 
-class UserUpdate(BaseDTO):
-    id: str
-    is_signed: bool | None = None
-    name: str | None = None
-    level: int | None = None
-    grade: int | None = None
-    part_index: int | None = None
-    profile_image: str | None = None
-    univ: str | None = None
-    last_cleaning: date | None = None
-    rest_type: int | None = None
-    etc_message: str | None = None
-    absent_reason: str | None = None
-    absent_detail_reason: str | None = None
-    phone_number: str | None = None
-    join_date: date | None = None
-    birth_date: date | None = None
-    birth_month: int | None = None
-    birth_day: int | None = None
-    major: str | None = None
-    student_id: str | None = None
-    is_next_birth: bool | None = None
-    return_plan_date: date | None = None
-    workshop_count: int | None = None
-    gogoma: str | None = None
-    warning_point: int | None = None
-    fcm_token: str | None = None
-    api_access_level: int | None = None
+class UserCreate(UserBase, UserID):
+    pass
 
 
-class UserDelete(BaseDTO):
-    id: str
+class UserUpdate(Partial(UserBase), UserID):
+    pass
+
+
+class UserDelete(UserID):
+    pass
