975553e0ac7adda7a77e949907b05a14f9d6c9da
reudekx <reudekx@gmail.com>
2024-12-22 15:38:16 +0900

    feat: Partial 함수 추가

--- app/dto/base.py
+++ app/dto/base.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel, ConfigDict
+from typing import Generic, Optional, TypeVar
+
+from pydantic import BaseModel, ConfigDict, create_model
 from sqlalchemy.orm import DeclarativeBase
 
 
@@ -11,7 +13,19 @@ class DTOMeta(type(BaseModel)):
 
 
 class BaseDTO(BaseModel, metaclass=DTOMeta):
-    model_config = ConfigDict(from_attributes=True, frozen=False, extra="allow")
+    model_config = ConfigDict(from_attributes=True, frozen=False)
 
     def dict(self, *args, **kwargs):
         return self.model_dump(*args, exclude_unset=True, **kwargs)
+
+
+DTO = TypeVar("DTO", bound=BaseDTO)
+
+
+def Partial(cls: type[DTO]) -> type[DTO]:
+    fields = {
+        field_name: (Optional[field_type.annotation], None)
+        for field_name, field_type in cls.model_fields.items()
+    }
+
+    return create_model(f"Partial{cls.__name__}", **fields)
