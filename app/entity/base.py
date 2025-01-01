ce59c0b26446f0668c811b21aa4b85c32ff91ec8
reudekx <reudekx@gmail.com>
2025-01-01 18:22:45 +0900

    feat: Entity 클래스 생성 시 문자열로 검색 가능하도록 사전에 저장

--- app/entity/base.py
+++ app/entity/base.py
@@ -1,8 +1,30 @@
+from typing import Type
+
 from pydantic import BaseModel
 from sqlalchemy.orm import DeclarativeBase
 
 
+class EntityRegistry:
+    _registry: dict[str, Type["BaseEntity"]] = {}
+
+    @classmethod
+    def register(cls, entity_cls: Type["BaseEntity"]):
+        cls._registry[entity_cls.__tablename__] = entity_cls
+
+    @classmethod
+    def __getitem__(cls, table_name: str) -> Type["BaseEntity"]:
+        return cls._registry[table_name]
+
+
 class EntityMeta(type(DeclarativeBase)):
+    def __new__(mcs, name, bases, attrs):
+        cls = super().__new__(mcs, name, bases, attrs)
+
+        if "__tablename__" in attrs:
+            EntityRegistry.register(cls)
+
+        return cls
+
     def __call__(cls, *args, **kwargs):
         if len(args) == 1 and isinstance(args[0], BaseModel):
             instance = cls(**args[0].model_dump(exclude_unset=True))
