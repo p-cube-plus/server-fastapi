2413765301eff45bebfd9d8d322e91c9cec4504b
reudekx <reudekx@gmail.com>
2025-01-01 11:15:33 +0900

    feat: user_attendance로의 테이블명 변경에 따른 수정

--- app/api/attendance/__init__.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from . import attendance, user
-
-router = APIRouter()
-router.include_router(attendance.router, tags=["attendance"])
-router.include_router(user.router, tags=["attendance > user"])
